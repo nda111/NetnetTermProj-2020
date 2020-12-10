971fe965be50bd53c05ce6bb01f36c0a8b8b096d
nda111 <nda111@naver.com>
2020-12-10 23:31:48 +0900

    Create Client.java

---
+++ src/app/Client.java
@@ -0,0 +1,35 @@
+package app;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import gui.SignInWindow;
+
+public class Client {
+	
+	public static void main(String[] args) {
+		
+		Socket socket = null;
+		String host = null;
+		int port = -1;
+
+		try {
+
+			File file = new File("./server_info.txt");			
+			Scanner scanner = new Scanner(file);
+
+			host = scanner.nextLine();
+			port = Integer.parseInt(scanner.nextLine());
+			scanner.close();
+			
+			socket = new Socket(host, port);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+			
+			System.exit(0);
+		}
+	}
+}
