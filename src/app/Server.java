48291149e6cb6afd311d8d5dd3bea5e57e3d5474
nda111 <nda111@naver.com>
2020-12-15 15:12:15 +0900

    Add: EditInfoResponse

--- src/app/Server.java
+++ src/app/Server.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 import data.ERequest;
+import data.EResponse;
 import data.Room;
 import data.User;
 import interaction.IResponse;
@@ -17,6 +18,7 @@ import interaction.response.AskFriendResponse;
 import interaction.response.AskUidResponse;
 import interaction.response.ByeChatResponse;
 import interaction.response.EchoResponse;
+import interaction.response.EditInfoResponse;
 import interaction.response.QuitResponse;
 import interaction.response.SayChatResponse;
 import interaction.response.SignInResponse;
@@ -29,14 +31,14 @@ import interaction.response.WhoAmIResponse;
 
 public class Server {
 	
-	public static final HashMap<ERequest, IResponse> Responses = new HashMap<>(); // 응답 리스
+	public static final HashMap<ERequest, IResponse> Responses = new HashMap<>(); // �쓳�떟 由ъ뒪
 	
-	public static final HashMap<String, User> Users = new HashMap<>(); // 사용자 리스트
+	public static final HashMap<String, User> Users = new HashMap<>(); // �궗�슜�옄 由ъ뒪�듃
 	
-	public static final HashMap<String, PrintWriter> Announcers = new HashMap<>(); // 출력스트림 
+	public static final HashMap<String, PrintWriter> Announcers = new HashMap<>(); // 異쒕젰�뒪�듃由� 
 	
-	public static final HashMap<String, String> PendingChats = new HashMap<>();	// 수락/거절 대기 중인 채팅방의 요청자-수신자 쌍 
-	public static final HashMap<Integer, Room> ChatRooms = new HashMap<>(); // 대화 중인 채팅방 
+	public static final HashMap<String, String> PendingChats = new HashMap<>();	// �닔�씫/嫄곗젅 ��湲� 以묒씤 梨꾪똿諛⑹쓽 �슂泥��옄-�닔�떊�옄 �뙇 
+	public static final HashMap<Integer, Room> ChatRooms = new HashMap<>(); // ���솕 以묒씤 梨꾪똿諛� 
 	
 	public static ServerSocket server = null;
 	
@@ -47,7 +49,7 @@ public class Server {
 		
 		try {
 
-			// 포트넘버를 받아온다
+			// �룷�듃�꽆踰꾨�� 諛쏆븘�삩�떎
 			String Port = null;
 			File file = new File("./server_info.txt");
 			Scanner scanner = new Scanner(file);
@@ -56,15 +58,15 @@ public class Server {
 			scanner.close();
 
 			server = new ServerSocket(port);
-			System.out.println("서버준비완료");
+			System.out.println("�꽌踰꾩�鍮꾩셿猷�");
 
 			while (true) {
 
 				Socket client = server.accept();
 				
-				ServerResponser handler = new ServerResponser(client); // 스레드 생성
+				ServerResponser handler = new ServerResponser(client); // �뒪�젅�뱶 �깮�꽦
 				Thread thread = new Thread(handler);
-				thread.start(); // 스레드 시작
+				thread.start(); // �뒪�젅�뱶 �떆�옉
 			} // while
 		} catch (IOException e) {
 
@@ -99,6 +101,9 @@ public class Server {
 		Responses.put(ERequest.ACK_CHAT, new AckChatResponse());
 		Responses.put(ERequest.SAY_CHAT, new SayChatResponse());
 		Responses.put(ERequest.END_CHAT, new ByeChatResponse());
+		
+		// Edit info
+		Responses.put(ERequest.EDIT_INF, new EditInfoResponse());
 	}
 	
 	// Load all user info from files before the server starts.
